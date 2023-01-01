b9e607771987014cd51381b3025812f9c6611fe8
mepanijaimini <20bca024@sanskareducation.org>
2023-01-01 10:11:59 +0530

    initial commit

--- app/src/main/java/com/example/e_udhyog/BuyerLogin.java
+++ app/src/main/java/com/example/e_udhyog/BuyerLogin.java
@@ -1,6 +1,9 @@
 package com.example.e_udhyog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class BuyerLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.buyer_login);
+        TextView  ForgotPassword = findViewById(R.id.forgotpassword);
+
+        ForgotPassword.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(BuyerLogin.this,ForgotPassword.class);
+                startActivity(intent);}
+        });
     }
 }
