c8102a1d155dba0ab578c369f82ee120cc5c3d01
mepanijaimini <20bca024@sanskareducation.org>
2023-01-03 17:27:25 +0530

    initial commit

--- app/src/main/java/com/example/e_udhyog/AlreadySelling.java
+++ app/src/main/java/com/example/e_udhyog/AlreadySelling.java
@@ -10,5 +10,8 @@ public class AlreadySelling extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.already_selling);
+
+
+
     }
 }
